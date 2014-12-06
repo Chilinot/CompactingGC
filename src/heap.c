71700174d6f4334db16deb0723cb3826d7d22cf9
Lucas Arnström <lucasarnstrom@gmail.com>
2014-12-06 19:12:49 +0100

    movd heap.c, added structs from idea

---
+++ src/heap.c
@@ -0,0 +1,34 @@
+//#include <stdio.h>
+//#include <stdlib.h>
+
+typedef struct heap_block {
+	size_t block_length;
+	void *data;
+	HeapBlock next_block;
+} *HeapBlock;
+
+typedef struct heap_s {
+	HeapBlock active;
+	HeapBlock passive;
+} *Heap;
+
+//typedef struct heap_s{
+//  void* data;
+//  int var;
+//  struct heap_s* left;
+//  struct heap_s* right;
+//} heap_t;
+//
+//
+//heap_t* init(size_t bytes){
+//  heap_t* heap = malloc(sizeof(bytes));
+//  return heap;
+//}
+//
+//
+//int main(int argc, char* argv[]){
+//  printf("Hur stor heap vill du ha?");
+//  heap_t* ret = init(argv[1]);
+//
+//  return 0;
+//}
