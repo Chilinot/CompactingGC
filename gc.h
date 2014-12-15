2e55230375d7395a003a47a439f7e98516744f64
Lucas Arnström <lucasarnstrom@gmail.com>
2014-12-15 13:41:20 +0100

    Refactored heap_t typedef

--- gc.h
+++ gc.h
@@ -8,15 +8,15 @@
 #define __gc__
 
 /// The opaque data type holding all the heap data
-typedef struct heap_t heap_s;
+typedef struct heap_s *Heap;
 
 /// The signature of the trace function 
-typedef void *(*trace_f)(heap_s *h, void *obj);
+typedef void *(*trace_f)(Heap h, void *obj);
 
 /// The signature of object-specific trace functions. It will be
 /// called for its specific objects, and be given a generic trace
 /// function f to be called on each pointer inside obj.
-typedef void *(*s_trace_f)(heap_s *h, trace_f f, void *obj);
+typedef void *(*s_trace_f)(Heap h, trace_f f, void *obj);
 
 /// Create a new heap with bytes total size (including both spaces
 /// and metadata), meaning strictly less than bytes will be
@@ -24,18 +24,18 @@ typedef void *(*s_trace_f)(heap_s *h, trace_f f, void *obj);
 ///
 /// \param bytes the total size of the heap in bytes
 /// \return the new heap
-heap_s *h_init(size_t bytes);
+Heap h_init(size_t bytes);
 
 /// Delete a heap.
 ///
 /// \param h the heap
-void h_delete(heap_s *h);
+void h_delete(Heap h);
 
 /// Delete a heap and trace, killing off stack pointers.
 ///
 /// \param h the heap 
 /// \param dbg_value a value to be written into every pointer into h on the stack
-void h_delete_dbg(heap_s *h, void *dbg_value);
+void h_delete_dbg(Heap h, void *dbg_value);
 
 /// Allocate a new object on a heap with a given format string.
 ///
@@ -49,7 +49,7 @@ void h_delete_dbg(heap_s *h, void *dbg_value);
 /// \return the newly allocated object
 ///
 /// Note: the heap does *not* retain an alias to layout.
-void *h_alloc_struct(heap_s *h, char *layout);
+void *h_alloc_struct(Heap h, char *layout);
 
 /// Allocate a new object on a heap with a given size, and
 /// object-specific trace function.
@@ -60,7 +60,7 @@ void *h_alloc_struct(heap_s *h, char *layout);
 /// \param bytes the size in bytes
 /// \param f the object-specific trace function
 /// \return the newly allocated object
-void *h_alloc_union(heap_s *h, size_t bytes, s_trace_f f);
+void *h_alloc_union(Heap h, size_t bytes, s_trace_f f);
 
 /// Allocate a new object on a heap with a given size.
 ///
@@ -70,7 +70,7 @@ void *h_alloc_union(heap_s *h, size_t bytes, s_trace_f f);
 /// \param h the heap
 /// \param bytes the size in bytes
 /// \return the newly allocated object
-void *h_alloc_data(heap_s *h, size_t bytes);
+void *h_alloc_data(Heap h, size_t bytes);
 
 /// Manually trigger garbage collection.
 ///
@@ -79,12 +79,12 @@ void *h_alloc_data(heap_s *h, size_t bytes);
 ///
 /// \param h the heap
 /// \return the number of bytes collected
-size_t h_gc(heap_s *h);
+size_t h_gc(Heap h);
 
 /// Returns the available consecutive free memory. 
 ///
 /// \param h the heap
 /// \return the available consecutive free memory. 
-size_t h_avail(heap_s *h);
+size_t h_avail(Heap h);
 
 #endif
