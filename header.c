2a9b522d80cedccce744fd672673eff19c1f756f
Tony Wang <towa4489@tiselius.it.uu.se>
2014-12-16 13:59:09 +0100

    changed some integers to bit representation

--- header.c
+++ header.c
@@ -1,26 +1,29 @@
 #include <stdint.h>
 #include "header.h"
 
+
+
 void* header_clearHeaderTypeBits(void* header) {
 	intptr_t cast_header = (intptr_t) header;
-	cast_header &= ~(1 << 0);
-	cast_header &= ~(1 << 1);
-	return (void*) cast_header;
+	//cast_header &= ~(1 << 0);
+	//cast_header &= ~(1 << 1);
+	cast_header &= ~(0b11); // sätter de två minst signifkanta bitarna noll
+	return (void*) cast_header; 
+	
 }
 
 header_type header_getHeaderType(void* header) {
 	intptr_t cast_header = (intptr_t) header;
 	
-	cast_header &= 3;
-	
+	cast_header &= 0b11; //tar ut de minst två signifikanta bitarna  
 	switch(cast_header) {
-		case 0:
+		case 0b00:
 			return POINTER_TO_STRING;
-		case 1:
+		case 0b01:
 			return FORWARDING_ADDRESS;
-		case 2:
+		case 0b10:
 			return FUNCTION_POINTER;
-		case 3:
+		case 0b11:
 			return BITVECTOR;
 		default:
 			return ERROR;
