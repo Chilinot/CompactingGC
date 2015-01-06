9096e15bf2a54bad36d71f97a5f6b8accbd60691
Slax32 <slax32@gmail.com>
2015-01-06 21:08:06 +0100

    ändra specifikationer till heapIterator

--- heapIterator.h
+++ heapIterator.h
@@ -1,5 +1,3 @@
-#include "linkedlist.h"
-
 #ifndef __heapiterator__
 #define __heapiterator__
 
@@ -11,6 +9,19 @@
  * @param heap - The heap to iterate over.
  * @return LinkedList of pointers to the heap.
  */
-LinkedList heapIterator(void* start, Heap heap);
+//LinkedList heapIterator(void* start, Heap heap);
+
+
+
+/**
+ Objekt flyttas till den passiva/nya delen av heapen om den inte redan är flyttad och markeras som flyttad.
+Om objektet redan är flyttat kommer det inte flyttas igen, addresen till objektet på passiva delen kommer fortfarande retuneras.
+Om objektet har pekare till andra objekt på heapen kommer dom också flyttas och pekarna ändras därefter. 
+@param h heapen vi jobbar på
+@param obj är en pekare till objekt på den aktiva heapen
+@return en pekare till det nya stället(på den passiva delen av heapen) objektet nu kan hittas
+ */
+void heapIterator(Heap heap, void *obj);
+
 
 #endif
