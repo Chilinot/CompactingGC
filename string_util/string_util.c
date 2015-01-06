f83d4ebe2261bdf69c3d80ce8be14f183a85f12f
Marcus Fröling <colamackan@hotmail.com>
2015-01-06 13:10:10 +0100

    Working on platform dependence

--- string_util/string_util.c
+++ string_util/string_util.c
@@ -82,181 +82,191 @@ int returnDigit(char* layout, int digitPos){
  *@return Returns 1 if true, else 0
  */
 
-int checkForLongOrDouble(char* layout){
+int checkForPointerDoubleLong(char* layout){
   for(int i = 0; i < strlen(layout); i++){
-    if(layout[i] == 'l' || layout[i] == 'd'){
-      return 1;
-    }
-  }
-  return 0;
-}
-      
-
-
-/**
- * Searches from current position after non-integer or until end of string and returns that
- * position
- *@param layout The formatstring
- *@param currentPos Current position in the formatstring.
- *@return Returns the position of the next non-integer of the string.
- */
-
-int newPos(char* layout, int currentPos){
-  int i;
-  int newPos;
-  for(i = currentPos; i < strlen(layout); i++){
-    if(isdigit(layout[i])){
+    if(sizeof(void*) == 8){
+      if(layout[i] == 'l' || layout[i] == 'd' || layout[i] == '*'){
+	return 1;
+      }
     }
     else{
-      newPos = i;
-      return newPos;
+      if(layout[i] == 'l' || layout[i] == 'd'){
+	return 1;
+      }
     }
   }
-  newPos = i;
-  return newPos;
+    return 0;
 }
 
-/**
- *Given a formatstring, converts the string into a headerstring that describes the memorylayout.
- *If no string is given, terminate the program.
- *@param The formatstring to be converted.
- *@return Returns a headerstring.
- */
+
+  /**
+   * Searches from current position after non-integer or until end of string and returns that
+   * position
+   *@param layout The formatstring
+   *@param currentPos Current position in the formatstring.
+   *@return Returns the position of the next non-integer of the string.
+   */
+
+  int newPos(char* layout, int currentPos){
+    int i;
+    int newPos;
+    for(i = currentPos; i < strlen(layout); i++){
+      if(isdigit(layout[i])){
+      }
+      else{
+	newPos = i;
+	return newPos;
+      }
+    }
+    newPos = i;
+    return newPos;
+  }
+
+  /**
+   *Given a formatstring, converts the string into a headerstring that describes the memorylayout.
+   *If no string is given, terminate the program.
+   *@param The formatstring to be converted.
+   *@return Returns a headerstring.
+   */
 
 
-char* formatStringToHeaderString(char* layout){
-  char* headerString;
-  int longOrDoubleExists;
-  int is64or32;
-  if(sizeof(void*) == 8){ // Check on which platform the program is run
-    is64or32 = 1;
-  }
-  if(sizeof(void*) == 4){
-    is64or32 = 0;
-  }
-  if(PLATFORM == 1){
-    longOrDoubleExists = checkForLongOrDouble(layout);
-    if(longOrDoubleExists == 1){
+  char* formatStringToHeaderString(char* layout){
+    char* headerString;
+    int longOrDoubleExists;
+    int is64or32;
+    if(sizeof(void*) == 8){
+      if(checkForPointerDoubleLong(layout) == 1){ // Check on which platform the program is run
       is64or32 = 1;
+      }
+      else{
+	is64or32 = 0;
+      }
     }
-    else{
+    if(sizeof(void*) == 4){
       is64or32 = 0;
     }
-  }
-  if(is64or32 == 1){
-    headerString = malloc(sizeof(layout)+1);
-  }
-  if(is64or32 == 0){
-    headerString = malloc(sizeof(layout)+1);
-  }
-  if(layout == NULL){
-    puts("No formatstring to allocate");
-    exit(0);
-  }
-  char* r = "r";
-  char* star = "*";
-  int length = strlen(layout);  
-  int value;
-
-
-  for(int newPosition = 0; newPosition < length; newPosition++){
-    if(isdigit(layout[newPosition])){
-      value = returnDigit(layout, newPosition); // value is the integer-value before a character
-      newPosition = newPos(layout, newPosition); // change position to next character if possible
-      if(length == newPosition){
-	concateFormatString(value, headerString, 1, r, is64or32);
-	return headerString;
+    if(PLATFORM == 1){
+      longOrDoubleExists = checkForPointerDoubleLong(layout);
+      if(longOrDoubleExists == 1){
+	is64or32 = 1;
+      }
+      else{
+	is64or32 = 0;
       }
     }
-    else{
-      value = 1; // default value
+    if(is64or32 == 1){
+      headerString = malloc(sizeof(layout)+1);
     }
-    if(layout[newPosition] == '*') {
-      int size = sizeof(void*);
-      concateFormatString(value, headerString, size, star, is64or32 ); 
+    if(is64or32 == 0){
+      headerString = malloc(sizeof(layout)+1);
     }
-    if(layout[newPosition] != 'c' && layout[newPosition] != '*' && layout[newPosition] != 'l' && layout[newPosition] != 'd' && layout[newPosition] != 'i' && layout[newPosition] != 'f'){
-      puts("Not a valid formatstring");
-      return NULL;
+    if(layout == NULL){
+      puts("No formatstring to allocate");
+      exit(0);
     }
-    if(layout[newPosition] == 'c') { // Check if several chars are in a row
-      if(sizeof(void*) == 4){
-	if (layout[newPosition + 1] == 'c'){
-	  newPosition++;
-	}
-	if (layout[newPosition + 1] == 'c'){
-	  newPosition++;
-	}
-	if (layout[newPosition + 1] == 'c'){
-	  newPosition++;
+    char* r = "r";
+    char* star = "*";
+    int length = strlen(layout);  
+    int value;
+
+
+    for(int newPosition = 0; newPosition < length; newPosition++){
+      if(isdigit(layout[newPosition])){
+	value = returnDigit(layout, newPosition); // value is the integer-value before a character
+	newPosition = newPos(layout, newPosition); // change position to next character if possible
+	if(length == newPosition){
+	  concateFormatString(value, headerString, 1, r, is64or32);
+	  return headerString;
 	}
       }
-      if(sizeof(void*) == 8){
-	if (layout[newPosition + 1] == 'c'){
-	  newPosition++;
+      else{
+	value = 1; // default value
+      }
+      if(layout[newPosition] == '*') {
+	int size = sizeof(void*);
+	concateFormatString(value, headerString, size, star, is64or32 ); 
+      }
+      if(layout[newPosition] != 'c' && layout[newPosition] != '*' && layout[newPosition] != 'l' && layout[newPosition] != 'd' && layout[newPosition] != 'i' && layout[newPosition] != 'f'){
+	puts("Not a valid formatstring");
+	return NULL;
+      }
+      if(layout[newPosition] == 'c') { // Check if several chars are in a row
+	if(is64or32 == 0 ){
+	  if (layout[newPosition + 1] == 'c'){
+	    newPosition++;
+	  }
+	  if (layout[newPosition + 1] == 'c'){
+	    newPosition++;
+	  }
+	  if (layout[newPosition + 1] == 'c'){
+	    newPosition++;
+	  }
 	}
-	if (layout[newPosition + 1] == 'c'){
-	  newPosition++;
-	}
-	if (layout[newPosition + 1] == 'c'){
-	  newPosition++;
-	}
-	if (layout[newPosition + 1] == 'c'){
-	  newPosition++;
-	}
-	if (layout[newPosition + 1] == 'c'){
-	  newPosition++;
-	}
-	if (layout[newPosition + 1] == 'c'){
-	  newPosition++;
-	}
+	if(is64or32 == 1 ){
+	  if (layout[newPosition + 1] == 'c'){
+	    newPosition++;
+	  }
+	  if (layout[newPosition + 1] == 'c'){
+	    newPosition++;
+	  }
+	  if (layout[newPosition + 1] == 'c'){
+	    newPosition++;
+	  }
+	  if (layout[newPosition + 1] == 'c'){
+	    newPosition++;
+	  }
+	  if (layout[newPosition + 1] == 'c'){
+	    newPosition++;
+	  }
+	  if (layout[newPosition + 1] == 'c'){
+	    newPosition++;
+	  }
 	
-	if (layout[newPosition + 1] == 'c'){
-	  newPosition++;
+	  if (layout[newPosition + 1] == 'c'){
+	    newPosition++;
+	  }
 	}
+	int size = sizeof(char);
+	concateFormatString(value, headerString, size, r, is64or32); 
       }
-      int size = sizeof(char);
-      concateFormatString(value, headerString, size, r, is64or32); 
+      if(layout[newPosition] == 'l') {
+	int size = sizeof(long);
+	concateFormatString(value, headerString, size, r, is64or32); 
+      }
+      if(layout[newPosition] == 'i'){
+	int size = sizeof(int);
+	concateFormatString(value, headerString, size, r, is64or32); 
+      }
+      if(layout[newPosition] == 'd') {
+	int size = sizeof(double);
+	concateFormatString(value, headerString, size, r, is64or32); 
+      }
+      if(layout[newPosition] == 'f') {
+	int size = sizeof(float);
+	concateFormatString(value, headerString, size, r, is64or32); 
+      }
     }
-    if(layout[newPosition] == 'l') {
-      int size = sizeof(long);
-      concateFormatString(value, headerString, size, r, is64or32); 
-    }
-    if(layout[newPosition] == 'i'){
-      int size = sizeof(int);
-      concateFormatString(value, headerString, size, r, is64or32); 
-    }
-    if(layout[newPosition] == 'd') {
-      int size = sizeof(double);
-      concateFormatString(value, headerString, size, r, is64or32); 
-    }
-    if(layout[newPosition] == 'f') {
-      int size = sizeof(float);
-      concateFormatString(value, headerString, size, r, is64or32); 
-    }
+    return headerString;
   }
-  return headerString;
-}
 
 
-size_t headerStringToSize(char* layout){
+  size_t headerStringToSize(char* layout){
   
-  switch(*layout){
-  case 'r':
-    return sizeof(int) + headerStringToSize(layout + 1);
-  case '*':
-    return sizeof(void*) + headerStringToSize(layout + 1);
-  case '\0':
+    switch(*layout){
+    case 'r':
+      return sizeof(int) + headerStringToSize(layout + 1);
+    case '*':
+      return sizeof(void*) + headerStringToSize(layout + 1);
+    case '\0':
+      return 0;
+    }
+
+    return 0; // should not happen
+
+  }
+
+  int main(int argc, char* argv[]){
+    char * test = formatStringToHeaderString(argv[1]);
+    printf("%s\n",test);
     return 0;
   }
-
-  return 0; // should not happen
-
-}
-/*
-int main(int argc, char* argv[]){
-  char * test = formatStringToHeaderString(argv[1]);
-  printf("%s\n",test);
-  return 0;
-}
-*/
