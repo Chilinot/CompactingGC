932821528d230a793e352ce034dd1b10234766f0
colamackan <colamackan@hotmail.com>
2015-01-13 15:10:13 +0100

    Update stack.h

--- stack.h
+++ stack.h
@@ -7,12 +7,12 @@
 
 
 /**
- *Finds all the possible pointers that points on the stack to the memoryspace where;
- *heapStart < memoryspace < heapEnd
- *and calls the function "foreach" for every pointer found.
- *@param heapStart pointer to the start of the heap
- *@param heapEnd pointer to the end of the heap
- *@param foreach function which is applied to all pointers found
+ * Finds all the possible pointers that points on the stack to the memoryspace where;
+ * heapStart < memoryspace < heapEnd
+ * and calls the function "foreach" for every pointer found.
+ * @param heapStart pointer to the start of the heap
+ * @param heapEnd pointer to the end of the heap
+ * @param foreach function which is applied to all pointers found
 */
 
 void stackIterator(void* heapStart, void* heapEnd,  void (*foreach)(void**));
