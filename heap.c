c411e63deda33c43a3a05b31915306a7089335f4
colamackan <colamackan@hotmail.com>
2015-01-13 14:06:55 +0100

    cleanup for doxygen

--- heap.c
+++ heap.c
@@ -12,6 +12,10 @@
 #include "debug.h"
 #endif
 
+// Creates a heap with the given size in bytes. Only half of the given amount will be usable at all times
+// due to it being split into an active and a passive area.
+// Returns null if the given size does not have space enough to fit metadata required by the heap.
+
 Heap heap_init(size_t bytes) {
 
 	size_t struct_size = sizeof(struct heap_s);
@@ -45,9 +49,14 @@ Heap heap_init(size_t bytes) {
 	return heap;
 }
 
+// De-allocates the given heap
+
 void heap_del(Heap heap) {
 	free(heap);
 }
+
+// Allocates a new object on the heap with the given amount of bytes in size.
+// This object is not allowed to have pointers back into the heap.
 
 void* heap_allocate_raw(Heap heap, size_t bytes) {
 	char string[bytes + 1];
@@ -59,6 +68,9 @@ void* heap_allocate_raw(Heap heap, size_t bytes) {
 	string[bytes] = '\0';
 	return heap_allocate_struct(heap, string);
 }
+
+// Allocates a new object on the heap with the given formatstring.
+// Valid characters are the same as for h_alloc_struct in gc.h
 
 void* heap_allocate_struct(Heap heap, char* structure) {
 	void* header = header_fromFormatString(heap, structure);
@@ -72,18 +84,27 @@ void* heap_allocate_struct(Heap heap, char* structure) {
 	return heap_allocateActive(heap, header, bytes);
 }
 
+// Allocates a new object on the heap with the given size
+// and object-specific trace function.
+
 void* heap_allocate_union(Heap heap, size_t bytes, s_trace_f f) {
 	void* header = header_objectSpecificFunction(f);
 	return heap_allocateActive(heap, header, bytes);
 }
 
+// Allocates the given amount of bytes on the given heap
+
 void* heap_allocateActive(Heap heap, void* header, size_t bytes) {
 	return heap_allocate(heap, header, bytes, &heap->active_pointer);
 }
 
+// Allocates the given amount of bytes on the given heap
+
 void* heap_allocatePassive(Heap heap, void* header, size_t bytes) {
 	return heap_allocate(heap, header, bytes, &heap->passive_pointer);
 }
+
+// Allocates the given amount of bytes on the given heap
 
 void* heap_allocate(Heap heap, void* header, size_t bytes, void** block_pointer) {
 	HeapBlock block = *block_pointer;
@@ -98,6 +119,10 @@ void* heap_allocate(Heap heap, void* header, size_t bytes, void** block_pointer)
 
 	return (((char*) &block->header) + sizeof(void*));
 }
+
+// Copies the data from the pointer pointing to a part in the active heap to the first empty slot
+// in the passive part. Will also mark the header of the object in the active part as copied to the
+// passive part.
 
 void* heap_copyFromActiveToPassive(Heap heap, void* data) {
 
@@ -156,6 +181,9 @@ void* heap_copyFromActiveToPassive(Heap heap, void* data) {
 	return (void*) passive_data;
 }
 
+// Replaced the header of the object pointed to by dataparameter with the given forwarding adress
+// and marks it as copied
+
 void heap_markAsCopied(void* data, void* forwarding_address) {
 	HeapBlock block = GET_HEAPBLOCK(data);
 
@@ -174,10 +202,14 @@ void heap_markAsCopied(void* data, void* forwarding_address) {
 #endif
 }
 
+// Checks if the object pointed to by the data parameter is marked as copied to the passive part.
+
 int heap_hasBeenCopied(void* data) {
 	HeapBlock block = GET_HEAPBLOCK(data);
 	return header_getHeaderType(block->header) == FORWARDING_ADDRESS;
 }
+
+// Marks the active part of the heap as the passive and vice versa.
 
 void heap_swapActiveAndPassive(Heap heap) {
 
@@ -192,6 +224,8 @@ void heap_swapActiveAndPassive(Heap heap) {
 	heap->passive_pointer = heap->passive;
 }
 
+// Returns the amount of bytes left that can be allocated.
+
 size_t heap_sizeLeft(Heap heap) {
 	size_t blocksize = heap->heapsize / 2;
 
@@ -203,9 +237,13 @@ size_t heap_sizeLeft(Heap heap) {
 	return blocksize - bytes_used;
 }
 
+// Returns pointer to the start of the active block in the given heap.
+
 void* heap_getActiveStart(Heap heap) {
 	return heap->active;
 }
+
+// Returns pointer to the end of the current allocated memory in the active heapblock of the given heap.
 
 void* heap_getActiveEnd(Heap heap) {
 	return heap->active_pointer;
