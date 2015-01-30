3c5dd238e685741af7e6bb6b43ac85ac5205f253
Lucas Arnström <lucasarnstrom@gmail.com>
2015-01-30 13:16:33 +0100

    Changed random seed and added comments

--- tests/stabilitytest/StabilityTest.c
+++ tests/stabilitytest/StabilityTest.c
@@ -3,17 +3,21 @@
 #include "binarytree.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 
 int main(void) {
 	// Initiate random generator with a random seed.
-	srand(9834589182300129);
+	srand(time(NULL));
 	
-	// Create a 2KB large heap for the binary tree.
+	// Create a 4KB large heap for the binary tree.
 	Heap heap = h_init(sizeof(struct heap_s) + 2048 * 2);
 	
 	printf("\nAvailable bytes in heap: %zu\n", h_avail(heap));
 	
+	// Initialize the tree with the key 25.
 	BinaryTree tree = tree_insert(heap, NULL, 25);
+	
+	// Add 50 random nodes to the tree.
 	for(int i = 0; i < 50; i++) {
 		tree = tree_insert(heap, tree, (rand() % 50) + 1);
 	}
